a2f1c4c6d20aff406e1d06d60acd12e825655a8e
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-12-03 14:20:55 -0500

    c

--- public/ver.js
+++ public/ver.js
@@ -1,4 +1,4 @@
-const v = 1.06
+const v = 1.07
 const vlog = function(v) {
 console.log(v)
 }
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,10 @@
 // vue.config.js
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          comments: false
-        }
-      })
-    ]
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.extractComments = true;
+      config.optimization.minimizer[0].options.uglifyOptions.output.comments = false;
+    }
   }
 };
